672bb1df0bfac53f49719d2547ad796924c92135
Emmanuel Olive <olive.emmanuel@gmail.com>
2016-05-30 10:37:59 +0200

    Inject container in the console application

--- bin/manage.php
+++ bin/manage.php
@@ -2,9 +2,24 @@
 
 require_once dirname(__FILE__)."/../app/bootstrap.php";
 
+use Moulino\Framework\Config\Config;
+use Moulino\Framework\Service\Config as ServiceConfig;
+use Moulino\Framework\Service\Loader as ServiceLoader;
 use Moulino\Framework\Console\Application;
 
-$app = new Application();
+Config::loadConfigFile(APP_CONFIG);
+
+$frameworkServiceConfig = new ServiceConfig(FRAMEWORK_SERVICES);
+$appServiceConfig 		= new ServiceConfig(APP_SERVICES);
+
+$svcLoader = new ServiceLoader();
+$svcLoader->load($frameworkServiceConfig);
+$svcLoader->load($appServiceConfig);
+$svcLoader->loadModels();
+
+$container = $svcLoader->getContainer();
+
+$app = new Application($container);
 $app->run();
 
 ?>
